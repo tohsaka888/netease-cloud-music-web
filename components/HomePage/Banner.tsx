73e7b8a6e1a2927c666ea0fc9957b655f643d53a
tohsaka888 <156132264@qq.com>
2022-11-09 16:56:31 +0800

    ✨ feat: Banner

--- components/HomePage/Banner.tsx
+++ components/HomePage/Banner.tsx
@@ -1,6 +1,6 @@
 import useBanner from "@services/useBanner";
 import { Carousel } from "antd";
-import React, { useRef } from "react";
+import React, { CSSProperties, useRef, useState } from "react";
 import { useMemo } from "react";
 import Image from "next/image";
 import { useResponsive } from "ahooks";
@@ -13,64 +13,111 @@ function Banner() {
   const banners = useMemo(() => data?.banners || [], [data]);
   const responsive = useResponsive();
   const carouselRef = useRef<CarouselRef>(null!);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  const imageSize = useMemo(() => {
+  // responsive styles
+  const styles = useMemo(() => {
+    const styles: Record<string, CSSProperties> = {
+      imageStyle: {
+        width: "730px",
+        height: `${730 * 0.38}px`,
+      },
+      downloadStyle: {
+        display: "block",
+        width: "253px",
+      },
+      arrowStyle: {
+        opacity: 1,
+      },
+    };
     if (responsive) {
       if (responsive.xl) {
-        return { width: 730, height: 730 * 0.38 };
+        styles.imageStyle.width = "730px";
       } else if (responsive.lg) {
-        return { width: 680, height: 680 * 0.38 };
+        styles.imageStyle.width = "600px";
+      } else if (responsive.md) {
+        styles.imageStyle.width = "680px";
+        styles.downloadStyle = {
+          display: "none",
+          width: "0px",
+        };
       } else {
-        return { width: 630, height: 630 * 0.38 };
+        styles.imageStyle.width = "500px";
+        styles.arrowStyle.opacity = 0;
+        styles.downloadStyle = {
+          display: "none",
+          width: "0px",
+        };
       }
-    } else {
-      return { width: 730, height: 730 * 0.38 };
     }
+    return styles;
   }, [responsive]);
 
   return (
-    <div
-      style={{ width: "100vw", height: imageSize.height, position: "relative" }}
+    <motion.div
+      initial={{
+        width: "100vw",
+        height: styles.imageStyle.height,
+        position: "relative",
+        background: `url(${
+          banners[currentIndex].imageUrl + "?imageView&blur=40x20"
+        })`,
+        backgroundSize: "6000px",
+        backgroundPosition: "center center",
+      }}
+      animate={{
+        background: `url(${
+          banners[currentIndex].imageUrl + "?imageView&blur=40x20"
+        })`,
+      }}
     >
       <div
         style={{
-          width: "64vw",
+          width: "100vw",
           position: "relative",
-          margin: "0px 18vw",
+          padding: `0px calc((100vw - ${styles.imageStyle.width} - ${styles.downloadStyle.width})/ 2)`,
           overflow: "hidden",
         }}
       >
         <Carousel
-          style={{ ...imageSize }}
+          style={{
+            boxShadow: "0px 0px 50px 3px #333333",
+            ...styles.imageStyle,
+          }}
           autoplay
           autoplaySpeed={3000}
           effect={"fade"}
           ref={carouselRef}
+          beforeChange={(current, next) => {
+            setCurrentIndex(next);
+          }}
         >
           {banners.map((banner) => (
-            <React.Fragment key={banner.targetId}>
-              <div style={{ height: "300px", width: "50vw" }}>
-                <Image
-                  src={banner.imageUrl}
-                  alt={banner.imageUrl}
-                  priority
-                  width={imageSize.width}
-                  height={imageSize.height}
-                />
-              </div>
-            </React.Fragment>
+            <Image
+              key={banner.targetId}
+              src={banner.imageUrl}
+              alt={banner.imageUrl}
+              priority
+              width={parseInt(styles.imageStyle.width as string)}
+              height={parseInt(styles.imageStyle.height as string)}
+              style={{
+                boxShadow: "0px 0px 50px 3px #333333",
+              }}
+            />
           ))}
         </Carousel>
         <div
           style={{
             position: "absolute",
-            height: imageSize.height,
-            right: "0px",
+            height: styles.imageStyle.height,
+            left: `calc((100vw - ${styles.imageStyle.width} - ${styles.downloadStyle.width})/ 2 + ${styles.imageStyle.width})`,
             background:
               "url(https://s2.music.126.net/style/web2/img/index/download.png?5a1a8555a5ed4cc3f268392ace664ea8) no-repeat 0 0",
             top: "0px",
-            width: `calc(100% - ${imageSize.width}px)`,
-            boxShadow: "0px 0px 20px 3px #333333",
+            boxShadow: "0px 0px 50px 3px #333333",
+            zIndex: 10,
+            opacity: 1,
+            ...styles.downloadStyle,
           }}
         />
       </div>
@@ -78,31 +125,39 @@ function Banner() {
         initial={{
           position: "absolute",
           top: "calc(50% - 24px)",
-          left: "calc(9vw - 24px)",
           cursor: "pointer",
+          left: `calc((100vw - ${styles.imageStyle.width} - ${styles.downloadStyle.width}) / 4)`,
+        }}
+        animate={{
+          left: `calc((100vw - ${styles.imageStyle.width} - ${styles.downloadStyle.width}) / 4)`,
+          opacity: styles.arrowStyle.opacity,
         }}
         whileHover={{
           opacity: 0.5,
         }}
         onClick={() => carouselRef.current.prev()}
       >
-        <BsArrowLeftShort size={56} />
+        <BsArrowLeftShort size={56} color={"#dfdfdf"} />
       </motion.div>
       <motion.div
         initial={{
           position: "absolute",
           top: "calc(50% - 24px)",
-          right: "calc(9vw - 24px)",
           cursor: "pointer",
+          right: `calc((100vw - ${styles.imageStyle.width} - ${styles.downloadStyle.width}) / 4)`,
+        }}
+        animate={{
+          right: `calc((100vw - ${styles.imageStyle.width} - ${styles.downloadStyle.width}) / 4)`,
+          opacity: styles.arrowStyle.opacity,
         }}
         whileHover={{
           opacity: 0.5,
         }}
         onClick={() => carouselRef.current.next()}
       >
-        <BsArrowRightShort size={56} />
+        <BsArrowRightShort size={56} color={"#dfdfdf"} />
       </motion.div>
-    </div>
+    </motion.div>
   );
 }
 
