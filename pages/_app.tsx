6136a93ed89d4e084a1bb4daad37b603e9e7ed85
tohsaka888 <156132264@qq.com>
2022-11-09 16:58:04 +0800

    ✨ feat: config swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { SWRConfig } from "swr";
+import { message } from "antd";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <SWRConfig
+      value={{
+        onError(err: Error, key, config) {
+          message.error(err.name + err.message);
+          if (!router.pathname.includes("error")) {
+            router.push("/error");
+          }
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
