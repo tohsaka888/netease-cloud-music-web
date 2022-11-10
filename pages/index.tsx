9be6e100288cdf76248831c6221c705366a4c1a1
tohsaka888 <156132264@qq.com>
2022-11-10 17:48:29 +0800

    ✨ feat(ssr): fetch recommed data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
-import { getBanner, url } from "@services/useBanner";
+import { getBanner, url as bannerUrl } from "@services/useBanner";
+import {
+  getHotRecommend,
+  url as hotRecommendUrl,
+} from "@services/useHotRecommend";
 import Navbar from "components/Common/Navbar";
 import Banner from "components/HomePage/Banner";
+import Content from "components/HomePage/Content";
 import { NextPage } from "next";
 import { SWRConfig } from "swr";
 
@@ -9,16 +14,19 @@ const Home: NextPage<{ fallback: Promise<any> }> = ({ fallback }) => {
     <SWRConfig value={{ fallback }}>
       <Navbar />
       <Banner />
+      <Content />
     </SWRConfig>
   );
 };
 
 export const getServerSideProps = async () => {
   const bannerData = await getBanner();
+  const hotRecommendData = await getHotRecommend();
   return {
     props: {
       fallback: {
-        [url]: bannerData,
+        [bannerUrl]: bannerData,
+        [hotRecommendUrl]: hotRecommendData,
       },
     },
   };
