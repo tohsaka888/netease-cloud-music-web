7cd42bf79a48190881626c19e54ead59b4132537
tohsaka888 <156132264@qq.com>
2022-11-11 17:36:27 +0800

    ✨ feat(services): fetch playlist data

---
+++ hooks/services/usePlaylistDetail.tsx
@@ -0,0 +1,35 @@
+import { baseUrl } from "config/baseUrl";
+import React from "react";
+import useSWR from "swr";
+
+import { PlaylistDetailResponse } from "types";
+
+export const url = `${baseUrl}/playlist/detail`;
+
+type RequestProps = {
+  id: number;
+  s?: number;
+};
+
+export const getPlaylistDetail = async ({ id, s }: RequestProps) => {
+  const res = await fetch(url + `?id=${id}`, {
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, s }),
+    method: "POST",
+  });
+  const data: PlaylistDetailResponse = await res.json();
+  return data;
+};
+
+function usePlaylistDetail({ id, s }: RequestProps) {
+  const response = useSWR(
+    id.toString(),
+    async () => await getPlaylistDetail({ id, s })
+  );
+  return response;
+}
+
+export default usePlaylistDetail;
