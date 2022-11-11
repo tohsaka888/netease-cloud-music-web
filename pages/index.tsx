1629101b84f864dfa6e99d4557e05d12f8ac538b
tohsaka888 <156132264@qq.com>
2022-11-11 17:37:32 +0800

    ✨ feat(ssr): fetch playlist data in server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import {
   url as hotRecommendUrl,
 } from "@services/useHotRecommend";
 import { url as newAlbumUrl, getNewAlbum } from "@services/useNewAlbum";
+import { getPlaylistDetail } from "@services/usePlaylistDetail";
 import Navbar from "components/Common/Navbar";
 import Banner from "components/HomePage/Banner";
 import Content from "components/HomePage/Content";
+import { newId, originId, solarId } from "config/toplistIds";
 import { NextPage } from "next";
 import { SWRConfig } from "swr";
 
@@ -24,12 +26,18 @@ export const getServerSideProps = async () => {
   const bannerData = await getBanner();
   const hotRecommendData = await getHotRecommend();
   const newAlbumData = await getNewAlbum();
+  const soarListData = await getPlaylistDetail({ id: +solarId! });
+  const newListData = await getPlaylistDetail({ id: +newId! });
+  const originListData = await getPlaylistDetail({ id: +originId! });
   return {
     props: {
       fallback: {
         [bannerUrl]: bannerData,
         [hotRecommendUrl]: hotRecommendData,
         [newAlbumUrl]: newAlbumData,
+        [`${solarId}`]: soarListData,
+        [`${newId}`]: newListData,
+        [`${originId}`]: originListData,
       },
     },
   };
