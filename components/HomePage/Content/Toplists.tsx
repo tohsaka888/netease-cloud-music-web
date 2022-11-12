ab0f9243d37d5e91295b624c0deed4c9c830ab92
tohsaka888 <156132264@qq.com>
2022-11-12 11:05:08 +0800

    ✨ feat: toplists

--- components/HomePage/Content/Toplists.tsx
+++ components/HomePage/Content/Toplists.tsx
@@ -5,6 +5,11 @@ import { newId, originId, solarId } from "config/toplistIds";
 import React, { useMemo } from "react";
 import PartTitle from "./PartTitle";
 import { Playlist } from "types";
+import globalStyle from "styles/global.module.css";
+import styles from "../index.module.css";
+import MultipleLines from "components/Common/MultipleLines";
+import Image from "next/image";
+import NeteaseIcon from "components/Common/NeteaseIcons";
 
 function Toplists() {
   const { data: soarData } = usePlaylistDetail({ id: +solarId! });
@@ -27,40 +32,101 @@ function Toplists() {
           },
         });
       }
+      res.push({
+        soar: { name: "查看全部>" },
+        new: { name: "查看全部>" },
+        origin: { name: "查看全部>" },
+      });
     }
     return res;
   }, [soarData, newData, originData]);
 
+  const render = (
+    val: Playlist,
+    record: Record<string, Playlist>,
+    index: number
+  ) => {
+    return (
+      <>
+        {val.name.includes("查看全部") ? (
+          <div className={styles["read-more"]}>{val.name}</div>
+        ) : (
+          <div className={globalStyle["flex-align-center"]}>
+            <div
+              className={styles["top-index"]}
+              style={{ color: index <= 2 ? "#c10d0c" : "#666" }}
+            >
+              {index + 1}
+            </div>
+            <MultipleLines
+              lines={1}
+              maxWidth={"100%"}
+              style={{
+                flex: 1,
+                cursor: "pointer",
+                userSelect: "none",
+                fontSize: "12px",
+              }}
+              className={styles["toplist-song-name"]}
+            >
+              {val.name}
+            </MultipleLines>
+          </div>
+        )}
+      </>
+    );
+  };
+
+  const columnTitle = (playlist: Playlist) => {
+    return (
+      <div className={styles["column-title-container"]}>
+        <Image
+          src={playlist.coverImgUrl}
+          alt={playlist.coverImgId_str}
+          width={80}
+          height={80}
+        />
+        <div className={styles["playlist-mask"]} />
+        <div className={styles["column-title-playlist-container"]}>
+          <div>{playlist.name}</div>
+          <div className={globalStyle["flex-align-center"]}>
+            <NeteaseIcon
+              name={"play"}
+              width={22}
+              height={22}
+              style={{ marginTop: "8px", marginRight: "16px" }}
+            />
+            <NeteaseIcon
+              name={"collect"}
+              width={22}
+              height={22}
+              style={{ marginTop: "8px" }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const columns: ColumnType<any>[] = useMemo(() => {
     return [
       {
-        title: <span>飙升榜</span>,
+        title: columnTitle(soarData!.playlist),
         key: "soar",
         dataIndex: "soar",
-        render(val: Playlist, record, index) {
-          return <span>{val.name}</span>;
-        },
+        render,
       },
       {
-        title: <span>新歌榜</span>,
+        title: columnTitle(newData!.playlist),
         key: "new",
         dataIndex: "new",
-        render(val: Playlist, record, index) {
-          return (
-            <span>
-              {index + 1}
-              {val.name}
-            </span>
-          );
-        },
+        render,
       },
       {
-        title: <span>原创榜</span>,
+        title: columnTitle(originData!.playlist),
         key: "origin",
         dataIndex: "origin",
-        render(val: Playlist, record, index) {
-          return <span>{val.name}</span>;
-        },
+        render,
       },
     ];
   }, [soarData, newData, originData]);
@@ -68,7 +134,13 @@ function Toplists() {
   return (
     <>
       <PartTitle>榜单</PartTitle>
-      <Table columns={columns} dataSource={dataSource} />
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={false}
+        className={"toplist-table"}
+        bordered
+      />
     </>
   );
 }
