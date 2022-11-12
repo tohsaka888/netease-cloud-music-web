164dc73195c7a8ea784bb1c6b1f669b96a0549c2
tohsaka888 <156132264@qq.com>
2022-11-12 11:46:34 +0800

    🦄 refactor: fefactor icon props and effect

--- components/Common/NeteaseIcons.tsx
+++ components/Common/NeteaseIcons.tsx
@@ -4,19 +4,24 @@ import { motion } from "framer-motion";
 const iconImage =
   "https://s2.music.126.net/style/web2/img/index/index.png?724237747bf71331ed93400836cc97b7";
 
-type IconName = "play" | "collect";
+const iconImageSmall =
+  "https://s2.music.126.net/style/web2/img/icon.png?e4400778139cc89f6897237ffc454470";
+
+type IconName = "play" | "collect" | "add" | "play-small" | "collect-small";
 
 type IconPositionProps = Record<
   IconName,
   {
     position: string;
     activePosition: string;
+    width: number;
+    height: number;
+    background: string;
   }
 >;
 
 type Props = {
-  width: number;
-  height: number;
+  size?: number;
   name: IconName;
 } & HTMLAttributes<HTMLDivElement>;
 
@@ -24,23 +29,50 @@ const iconPosition: IconPositionProps = {
   play: {
     position: "-267px -205px",
     activePosition: "-267px -235px",
+    width: 22,
+    height: 22,
+    background: iconImage,
   },
   collect: {
     position: "-300px -205px",
     activePosition: "-300px -235px",
+    width: 22,
+    height: 22,
+    background: iconImage,
+  },
+  add: {
+    position: "0 -700px",
+    activePosition: "-22px -700px",
+    width: 17,
+    height: 17,
+    background: iconImageSmall,
+  },
+  "play-small": {
+    position: "-267px -268px",
+    activePosition: "-267px -288px",
+    width: 17,
+    height: 17,
+    background: iconImage,
+  },
+  "collect-small": {
+    position: "-297px -268px",
+    activePosition: "-297px -288px",
+    width: 17,
+    height: 17,
+    background: iconImage,
   },
 };
 
-function NeteaseIcon({ width, height, name, ...props }: Props) {
+function NeteaseIcon({ size = 1, name, ...props }: Props) {
   return (
-    <div {...props}>
+    <div {...props} style={{ transform: `scale(${size})`, ...props.style }}>
       <motion.div
         initial={{
-          background: `url(${iconImage}) no-repeat 0 9999px`,
+          background: `url(${iconPosition[name].background}) no-repeat 0 9999px`,
           backgroundPosition: iconPosition[name].position,
-          width,
-          height,
           opacity: 1,
+          width: iconPosition[name].width,
+          height: iconPosition[name].height,
         }}
         whileHover={{
           backgroundPosition: iconPosition[name].activePosition,
