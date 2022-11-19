41b5b4fda2687559af127eaef3a7ba3c90909cf0
tohsaka888 <156132264@qq.com>
2022-11-19 12:18:17 +0800

    ✨ feat: sider area

--- components/HomePage/Sider/index.tsx
+++ components/HomePage/Sider/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Layout } from "antd";
+import useHomePageStyles from "hooks/styles/useHomePageStyles";
+import React from "react";
+import Artists from "./Artists";
+import SuggestLogin from "./SuggestLogin";
 
 function Sider() {
+  const { imageStyle, downloadStyle } = useHomePageStyles();
   return (
-    <div>Sider</div>
-  )
+    <Layout.Sider
+      style={{
+        marginRight: `calc((100% - ${imageStyle.width} - ${downloadStyle.width}) / 2)`,
+        border: "1px solid #d3d3d3",
+        ...downloadStyle,
+      }}
+      width={downloadStyle.width}
+      theme={"light"}
+    >
+      <SuggestLogin />
+      <Artists />
+    </Layout.Sider>
+  );
 }
 
-export default Sider
+export default Sider;
