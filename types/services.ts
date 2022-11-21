4c95347173a2cda4f2f7198472d8842523aa2e2c
tohsaka888 <156132264@qq.com>
2022-11-21 16:12:07 +0800

    🦄 refactor: add type

--- types/services.ts
+++ types/services.ts
@@ -30,3 +30,36 @@ export type PlaylistDetailResponse = {
   fromUserCount: number;
   songFromUsers: null;
 };
+
+type ArtistProps = {
+  name: string;
+  id: number;
+  picId: number;
+  img1v1Id: number;
+  briefDesc: string;
+  picUrl: string;
+  img1v1Url: string;
+  albumSize: number;
+  alias: string[];
+  trans: string;
+  musicSize: number;
+  topicPerson: number;
+  showPrivateMsg: null;
+  isSubed: null;
+  accountId: null | number;
+  picId_str: string;
+  img1v1Id_str: string;
+  transNames: null;
+  followed: boolean;
+  mvSize: null;
+  publishTime: null;
+  identifyTag: null;
+  alg: null;
+  fansCount: number;
+};
+
+export type HotArtistsResponse = {
+  code: number;
+  more: boolean;
+  artists: ArtistProps[];
+};
