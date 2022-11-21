24728893d4b560e242599ef11c071e48bb8a4d87
tohsaka888 <156132264@qq.com>
2022-11-21 16:11:40 +0800

    ✨ feat(services): fetch hot artist data

---
+++ hooks/services/useHotArtists.tsx
@@ -0,0 +1,18 @@
+import { baseUrl } from "config/baseUrl";
+import React from "react";
+
+import useImmutableSWR from "swr/immutable";
+import { HotArtistsResponse } from "types";
+
+export const getHotArtists = async () => {
+  const res = await fetch(`${baseUrl}/top/artists?offset=0&limit=5`);
+  const data: HotArtistsResponse = await res.json();
+  return data;
+};
+
+function useHotArtists() {
+  const response = useImmutableSWR("hotArtists", getHotArtists);
+  return response;
+}
+
+export default useHotArtists;
