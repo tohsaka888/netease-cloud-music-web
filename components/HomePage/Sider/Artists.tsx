949658909097c53ef50e4785b9dd2669ec092d92
tohsaka888 <156132264@qq.com>
2022-11-21 16:11:01 +0800

    ✨ feat: artists recommend in side bar

--- components/HomePage/Sider/Artists.tsx
+++ components/HomePage/Sider/Artists.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import useHotArtists from "@services/useHotArtists";
+import Image from "next/image";
+import React, { useMemo } from "react";
+import styles from "./index.module.css";
 
 function Artists() {
+  const { data } = useHotArtists();
+  const artists = useMemo(() => data?.artists, []);
+
   return (
-    <div>Artists</div>
-  )
+    <div className={styles["sider-container"]}>
+      <div className={styles["sider-title"]}>
+        <span style={{ fontWeight: "bold" }}>入驻歌手</span>
+        <span>查看全部{">"}</span>
+      </div>
+      {artists?.map((artist) => {
+        return (
+          <div className={styles["artist-container"]}>
+            <Image
+              width={62}
+              height={62}
+              alt={artist.img1v1Id_str}
+              src={artist.img1v1Url}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Artists
+export default Artists;
