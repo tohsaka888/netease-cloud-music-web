55a667587809e50bcf96ae0bbee1a621e3e8f7e6
tohsaka888 <156132264@qq.com>
2022-11-28 16:26:59 +0800

    🦄 refactor: fix fetch account info not with cookie

--- hooks/services/useAccountInfo.tsx
+++ hooks/services/useAccountInfo.tsx
@@ -3,9 +3,15 @@ import React from "react";
 import useSWR from "swr";
 
 export const getAccountInfo = async () => {
-  const res = await fetch(`${baseUrl}/user/account`, {
+  const timestamp = new Date().getTime();
+  const res = await fetch(`${baseUrl}/user/account?timestamp=${timestamp}`, {
     mode: "cors",
     credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ cookie: localStorage.getItem("cookie") }),
   });
   const data = await res.json();
   return data;
