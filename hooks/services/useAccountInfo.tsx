cf6ccde0e3084a389319adb8a6ece1ff3277f20e
tohsaka888 <156132264@qq.com>
2022-11-29 14:15:06 +0800

    ✨ feat(services): fetch account info

--- hooks/services/useAccountInfo.tsx
+++ hooks/services/useAccountInfo.tsx
@@ -1,24 +1,40 @@
 import { baseUrl } from "config/baseUrl";
-import React from "react";
+import React, { Key } from "react";
 import useSWR from "swr";
+import { AccountInfoResponse } from "types";
+import useLoginStatus from "./useLoginStatus";
 
-export const getAccountInfo = async () => {
+export const getAccountInfo = async (uid: Key) => {
   const timestamp = new Date().getTime();
-  const res = await fetch(`${baseUrl}/user/account?timestamp=${timestamp}`, {
+  const res = await fetch(`${baseUrl}/user/detail?timestamp=${timestamp}`, {
     mode: "cors",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
     method: "POST",
-    body: JSON.stringify({ cookie: localStorage.getItem("cookie") }),
+    body: JSON.stringify({ uid }),
   });
-  const data = await res.json();
-  return data;
+  if (res.ok) {
+    const data: AccountInfoResponse = await res.json();
+    return data;
+  } else {
+    throw new Error(
+      JSON.stringify({
+        statusCode: res.status,
+        title: res.statusText,
+        message: "can not fetch account info",
+      })
+    );
+  }
 };
 
 function useAccountInfo() {
-  const response = useSWR("accountInfo", getAccountInfo);
+  const { data: loginStatus } = useLoginStatus();
+  const uid = loginStatus?.data.account.id;
+  const response = useSWR(uid ? "accountInfo" : null, () => {
+    return getAccountInfo(uid!);
+  });
   return response;
 }
 
