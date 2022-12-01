bbc8fa394d72e504b1b04f74a99b3e337d1ea3c3
tohsaka888 <156132264@qq.com>
2022-12-01 16:31:22 +0800

    🎈 perf: force to rerender

--- components/My/Content.tsx
+++ components/My/Content.tsx
@@ -1,13 +1,15 @@
 import { Layout } from "antd";
 import PlaylistDetail from "components/Common/PlaylistDetail";
+import { useRouter } from "next/router";
 import React from "react";
 import styles from "./index.module.css";
 
 function Content() {
+  const { query } = useRouter();
   return (
     <Layout.Content className={styles["content-container"]}>
       <div className={styles["content-main"]}>
-        <PlaylistDetail />
+        <PlaylistDetail key={query.id as string} />
       </div>
     </Layout.Content>
   );
