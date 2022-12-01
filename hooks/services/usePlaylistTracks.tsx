e8469957bb40e473ad6b342ed32d63c71e5d30da
tohsaka888 <156132264@qq.com>
2022-12-01 16:35:16 +0800

    ✨ feat(services): fetch playlist tracks data

---
+++ hooks/services/usePlaylistTracks.tsx
@@ -0,0 +1,43 @@
+import { baseUrl } from "config/baseUrl";
+import React from "react";
+import useSWR from "swr";
+import { Track } from "types";
+
+type RequestProps = {
+  id: number;
+  page: number;
+  pageSize: number;
+};
+
+export const getPlaylistTracks = async ({
+  id,
+  page,
+  pageSize,
+}: RequestProps) => {
+  const res = await fetch(`${baseUrl}/playlist/track/all`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify({
+      id,
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    }),
+  });
+  const data = await res.json();
+  return data.songs;
+};
+
+function usePlaylistTracks(props: RequestProps) {
+  const response = useSWR<Track[]>(
+    `/playlist/track/all?id=${props.id}&limit=${props.pageSize}&offset=${
+      (props.page - 1) * props.pageSize
+    }`,
+    () => getPlaylistTracks(props)
+  );
+  return response;
+}
+
+export default usePlaylistTracks;
