b92ab4285e6a217aa4eb4e6d5108ff12e774ce3c
tohsaka888 <156132264@qq.com>
2022-12-02 19:02:01 +0800

    ✨ feat: add playlist knowledge graph

--- components/Common/PlaylistDetail.tsx
+++ components/Common/PlaylistDetail.tsx
@@ -1,5 +1,5 @@
 import usePlaylistDetail from "@services/usePlaylistDetail";
-import { Avatar, Button, Tag, Typography, Table } from "antd";
+import { Avatar, Button, Tag, Typography, Table, Modal } from "antd";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useMemo, useState } from "react";
@@ -12,6 +12,7 @@ import {
   BsCollection,
   BsDownload,
   BsFillChatLeftTextFill,
+  BsFillPinMapFill,
   BsPlayCircle,
   BsShare,
 } from "react-icons/bs";
@@ -19,6 +20,8 @@ import { ColumnType } from "antd/es/table";
 import { Ar, Track, Al } from "types";
 import NeteaseIcon from "./NeteaseIcons";
 import usePlaylistTracks from "@services/usePlaylistTracks";
+import { KnowledgeGraph } from "react-knowledge-graph";
+import useGraphData from "@services/useGraphData";
 
 const colors = [
   "magenta",
@@ -43,6 +46,8 @@ function PlaylistDetail() {
     page: 1,
     pageSize: 20,
   });
+  const [open, setOpen] = useState<boolean>(false);
+
   const { data: detail } = usePlaylistDetail({
     id: +(query.id as string),
   });
@@ -53,6 +58,7 @@ function PlaylistDetail() {
   const [ellipsis, setEllipsis] = useState<boolean>(true);
   const [isShowEllipsisButton, setIsShowEllipsisButton] =
     useState<boolean>(false);
+  const { explore } = useGraphData();
 
   const columns: ColumnType<Track>[] = useMemo(
     () => [
@@ -131,7 +137,7 @@ function PlaylistDetail() {
 
   return (
     <>
-      <div style={{ padding: "40px", display: "flex" }}>
+      <div style={{ padding: "40px", display: "flex", position: "relative" }}>
         <div className={styles["cover-image-container"]}>
           <Image
             width={200}
@@ -165,6 +171,71 @@ function PlaylistDetail() {
               {day(detail?.playlist.createTime!).format("YYYY-MM-DD")} 创建
             </div>
           </div>
+
+          <Button
+            type={"primary"}
+            style={{ position: "absolute", top: "75px", right: "10px" }}
+            onClick={() => setOpen(true)}
+          >
+            <BsFillPinMapFill
+              style={{ position: "absolute", left: "12px", top: "7px" }}
+            />
+            <div style={{ marginLeft: "16px" }}>图谱分析</div>
+          </Button>
+
+          <Modal
+            open={open}
+            title={"图谱分析"}
+            width={1200}
+            onCancel={() => setOpen(false)}
+            footer={null}
+          >
+            <KnowledgeGraph
+              basicDistence={80}
+              explore={explore}
+              enableAutoExplore={true}
+              node={{
+                id: query.id!.toString(),
+                name: detail!.playlist.name,
+                type: "歌单",
+                hasMore: true,
+                direction: "root",
+              }}
+              position={{
+                x: 600,
+                y: 260,
+              }}
+              width={"100%"}
+              height={580}
+              showFilter={false}
+              showNodeMenu={true}
+              typeConfig={{
+                ["收藏者"]: {
+                  radius: 18,
+                  typeSize: 8,
+                  nameSize: 8,
+                  nameColor: "#666",
+                  fill: "#89e8e6",
+                  hoverStyle: {
+                    fill: "#30e4e1",
+                  },
+                },
+                ["创建者"]: {
+                  radius: 22,
+                  fill: "#d389e8",
+                  hoverStyle: {
+                    fill: "#b130e4",
+                  },
+                },
+                ["歌曲"]: {
+                  fill: "#e8a189",
+                  hoverStyle: {
+                    fill: "#e46930",
+                  },
+                },
+              }}
+            />
+          </Modal>
 
           <div
             style={{ marginTop: "8px" }}
