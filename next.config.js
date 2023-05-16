90e7ebd5f750c6a9e1ff2b582c916856ab9e76d0
tohsaka888 <156132264@qq.com>
2023-05-16 18:19:07 +0800

    ✨ feat: enable swc minify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true,
+  swcMinify: true,
   images: {
     domains: ["p1.music.126.net", "s2.music.126.net", "p2.music.126.net", "p6.music.126.net"]
   }
